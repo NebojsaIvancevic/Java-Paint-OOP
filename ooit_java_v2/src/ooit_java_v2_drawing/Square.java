c0aececa6d6e032c1944c15c0264868fd8587c0b
Nebojsa Ivancevic <Nebojsa Ivancevic@DESKTOP-KS68BF5>
2020-08-17 04:00:23 -0700

    Initial commit

---
+++ ooit_java_v2/src/ooit_java_v2_drawing/Square.java
@@ -0,0 +1,8 @@
+package ooit_java_v2_drawing;
+
+public class Square extends Shape {
+
+	public void printSquare() {
+		System.out.println("Square is printing ....");
+	}
+}
---
+++ ooit_java_v2/src/ooit_java_v2_drawing/Test.java
@@ -0,0 +1,8 @@
+package ooit_java_v2_drawing;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Here we test...");
+	}
+}
